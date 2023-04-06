40cbfd8490373e457f91472b2bf3c4aeafde9820
Wallace Caldeira <wallace.cldr@gmail.com>
2023-04-06 11:03:17 -0300

    remove lombok use

--- src/main/java/br/com/udemy/phonebook/model/Phonebook.java
+++ src/main/java/br/com/udemy/phonebook/model/Phonebook.java
@@ -19,4 +19,68 @@ public class Phonebook {
     private String zipNumber;
     private String city;
     private String uf;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+
+    public String getZipNumber() {
+        return zipNumber;
+    }
+
+    public void setZipNumber(String zipNumber) {
+        this.zipNumber = zipNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
 }
